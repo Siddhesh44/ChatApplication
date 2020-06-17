81dc6dcf72ecd78924adcac96fbce383b09aeac6
Siddhesh <siddhesh.j@infiny.in>
2020-06-17 11:16:30 +0530

    Initial Commit

---
+++ ChatApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApplication
+//
+//  Created by Siddhesh jadhav on 17/06/20.
+//  Copyright © 2020 infiny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
